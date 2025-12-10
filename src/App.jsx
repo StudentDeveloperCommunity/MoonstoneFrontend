5082c530f2634c7a8a75f0c64260bf92b24db433
Taha <110016145+Tahacollege@users.noreply.github.com>
2025-12-11 00:22:23 +0530

    user section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
 import AllEvents from "./pages/AllEvents.jsx";
+import AboutUs from "./pages/Aboutus.jsx";
+import ClubDetails from "./pages/ClubDetails.jsx";
 
 const AppContent = () => {
   return (
@@ -11,6 +13,8 @@ const AppContent = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/allevents" element={<AllEvents />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/club" element={<ClubDetails />} />
       </Routes>
 
       <Footer />
